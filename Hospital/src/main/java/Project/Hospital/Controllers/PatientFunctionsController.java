7fa313706f0ca2541c55cf80fff6a6c931ba9696
MustafaMustafov <musitoahmed@gmail.com>
2023-03-08 11:55:29 +0200

    patient controller - reschedule/cancel appointments added

--- Hospital/src/main/java/Project/Hospital/Controllers/PatientFunctionsController.java
+++ Hospital/src/main/java/Project/Hospital/Controllers/PatientFunctionsController.java
@@ -2,25 +2,17 @@ package Project.Hospital.Controllers;
 
 import Project.Hospital.Entities.Appointment;
 import Project.Hospital.Entities.Doctor;
-import Project.Hospital.Entities.Patient;
 import Project.Hospital.Enums.AppointmentType;
 import Project.Hospital.Repositories.AppointmentRepository;
 import Project.Hospital.Repositories.DoctorRepository;
-import Project.Hospital.Repositories.PatientRepository;
-import Project.Hospital.Repositories.UserRepository;
 import Project.Hospital.Service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Optional;
 
 @Controller
 public class PatientFunctionsController {
@@ -58,6 +50,38 @@ public class PatientFunctionsController {
 
     @GetMapping("/edit-appointment")
     public String editAppointment(Model model) {
+        int id = patientService.loggedUserId();
+        model.addAttribute("appointments",appointmentRepository.findByPatientId(id));
         return "/patient/appointments/edit";
     }
+
+    @GetMapping("/cancel-appointment")
+    public String cancelAppointment(Model model) {
+        int id = patientService.loggedUserId();
+        model.addAttribute("appointments",appointmentRepository.findByPatientId(id));
+        return "/patient/appointments/cancel";
+    }
+
+    @PostMapping("/cancel-appointment/{appId}")
+    private ModelAndView deleteAppointment(@PathVariable(name="appId")Integer appId) {
+        System.out.println(appId);
+        appointmentRepository.deleteById(appId);
+        return new ModelAndView("redirect:/");
+    }
+
+    @GetMapping("/edit-appointment/{appId}")
+    public String editAppointment(@PathVariable(name="appId") Integer appId, Model model) {
+        model.addAttribute("doctorList",doctorRepository.findAll());
+        model.addAttribute("appointmentType",AppointmentType.values());
+        model.addAttribute("appointment",appointmentRepository.findById(appId));
+        return "/patient/appointments/edit-appointment";
+    }
+
+    @PostMapping("/update-appointment")
+    private ModelAndView updateAppointment(Appointment appointment) {
+        patientService.setAppointmentPatient(appointment);
+        appointmentRepository.save(appointment);
+        return new ModelAndView("redirect:/");
+    }
+
 }
